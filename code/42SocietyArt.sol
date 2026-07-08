30bfb9b079ee6c9fcbf262b201c5693c1ab71cf9
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-07-08 13:39:24 +0200

    WIP

--- code/42SocietyArt.sol
+++ code/42SocietyArt.sol
@@ -2,8 +2,13 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-import {ERC721, Ownable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * @notice using this interface to communicate with the FS42 token. The sole purpose of the communication is to check if a minter has a balance of the targeted token address
@@ -13,6 +18,10 @@ interface IERC20Balance {
     function balanceOf(address account) external view returns (uint256);
 }
 
-contract FortyTwoSocietyArt {
+contract FortyTwoSocietyArt is ERC721, Ownable {
+
+	constructor() ERC721("42SocietyGenArt", "F42A") Ownable(msg.sender){
+
+	}
 
 }
