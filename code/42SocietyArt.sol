973ba35bb3a603a2e9d1887a27eb5a0de1453675
Guillaume Ebersohl <gueberso@student.42lyon.fr>
2026-07-12 22:47:26 +0200

    feat(mandatory): 42SocietyArt smart contract - needs documentation

--- code/42SocietyArt.sol
+++ code/42SocietyArt.sol
@@ -2,26 +2,49 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
-
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * @notice using this interface to communicate with the FS42 token. The sole purpose of the communication is to check if a minter has a balance of the targeted token address
  * @dev Returns the token balance held by `account`. The address is meant to be the one of a token smart contract
  */
 interface IERC20Balance {
-    function balanceOf(address account) external view returns (uint256);
+	function balanceOf(address account) external view returns (uint256);
 }
 
-contract FortyTwoSocietyArt is ERC721, Ownable {
+/**
+ * @title 42SocietyArt - solution for the mandatory IPFS Contract
+ * @notice ERC-721 NFT gated by FS42 token ownership. Metadata stored on IPFS.
+ */
+contract FortyTwoSocietyArt is ERC721URIStorage, Ownable, ERC721 {
 
-	constructor() ERC721("42SocietyGenArt", "F42A") Ownable(msg.sender){
+	uint256 public constant MAX_SUPPLY = 9001;
+	uint256 private _tokenIdCounter;
 
+	IERC20Balance public immutable fs42Token;
+
+	string private constant METADATA_URI = "ipfs://bafkreigqppjjr2oi75t4dj2cun3fk3rimr4h756ylo5bmksvdgckhst3a4";
+
+	constructor(address _fs42Address) ERC721("42SocietyArt", "F42A") Ownable(msg.sender)
+	{
+		fs42Token = IERC20Balance(_fs42Address);
 	}
 
+	function mint() external {
+
+		require(fs42Token.balanceOf(msg.sender) > 0, "Must hold FS42 tokens");
+		require(_tokenIdCounter < MAX_SUPPLY, "Max supply reached");
+
+		uint256 tokenId = _tokenIdCounter;
+		_tokenIdCounter++;
+
+		_safeMint(msg.sender, tokenId);
+		_setTokenURI(tokenId, METADATA_URI);
+	}
+
+	function totalSupply() external view returns (uint256) {
+		return _tokenIdCounter;
+	}
 }
